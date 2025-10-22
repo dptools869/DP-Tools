8c956f8be170dc888d7f77cac74cced4c6eb2b0a
Sohaib Faisal Khan <sohaibfaisalkhan@gmail.com>
2025-10-22 14:04:57 +0000

    add this in header: <meta name="google-site-verification" content="CaLkn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { cn } from '@/lib/utils';
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta name="google-site-verification" content="CaLkncHEeHV74bNvHyhEpV2_di0q04srlWOIQVxYJ-o" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
